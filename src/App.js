71cf2da81eb6122743c4191fb37ba9452452863c
Rony <frrony1996@gmail.com>
2022-05-15 02:28:56 +0500

    preloader added

--- src/App.js
+++ src/App.js
@@ -7,20 +7,32 @@ import Login from './Pages/Shared/Login';
 import Footer from './Pages/Shared/Footer';
 import Signup from './Pages/Shared/Signup';
 import { ToastContainer } from 'react-toastify';
+import { useInView } from 'react-intersection-observer';
+import Loading from './Pages/Shared/Loading';
 
 
 function App() {
+  const { ref, inView } = useInView();
   return (
     <div className='bg-primary '>
-      <Header></Header>
-      <Routes>
-        <Route path='/' element={<Home></Home>}></Route>
-        <Route path='/home' element={<Home></Home>}></Route>
-        <Route path='/login' element={<Login></Login>}></Route>
-        <Route path='/signup' element={<Signup></Signup>}></Route>
-      </Routes>
-      <Footer></Footer>
-      <ToastContainer />
+      <div ref={ref}></div>
+      {
+        inView ?
+
+          <div>
+            <Header></Header>
+            <Routes>
+              <Route path='/' element={<Home></Home>}></Route>
+              <Route path='/home' element={<Home></Home>}></Route>
+              <Route path='/login' element={<Login></Login>}></Route>
+              <Route path='/signup' element={<Signup></Signup>}></Route>
+            </Routes>
+            <Footer></Footer>
+            <ToastContainer />
+          </div>
+          :
+          <Loading type="spokes" color="#EB4B98"></Loading>
+      }
     </div>
   );
 }
